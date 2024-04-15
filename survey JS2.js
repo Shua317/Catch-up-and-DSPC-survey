aa2b168947a4f214e945ed60fbe3e44840356849
Shua317 <157175620+Shua317@users.noreply.github.com>
2024-04-15 23:08:47 +0800

    Add files via upload

---
+++ survey JS2.js
@@ -0,0 +1,6 @@
+function One(){
+    var a = document.getElementById("name").value
+    var b = document.getElementById("section").value
+    var aa = localStorage.setItem("name",a)
+    var bb = localStorage.setItem("section",b)
+}
